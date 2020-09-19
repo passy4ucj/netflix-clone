6c1731ffac3bbc0bd83856be2bfb3f8f06dbc882
passy4ucj <ojinnnakapascal@gmail.com>
2020-09-19 05:41:49 -0700

    Project CleanUp

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+//a082dac20168021cd77111be28f02352
+function App() {
+  return (
+    <div className="app">
+      <h1>This is a Netflix Clone</h1>
+    </div>
+  );
+}
+
+export default App;
